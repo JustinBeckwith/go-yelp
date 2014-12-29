7598ace0c89d02c81d79a0d928ef25091a6e414a
Justin Beckwith <justbe@microsoft.com>
2014-12-28 23:45:40 -0800

    add location with coordinates test

--- yelp/locationOptions_test.go
+++ yelp/locationOptions_test.go
@@ -2,10 +2,12 @@ package yelp
 
 import (
 	"testing"
+
+	"github.com/guregu/null"
 )
 
 /**
- * Check using location options with bounding coordinates
+ * Check using location options by term
  */
 func TestLocationOptions(t *testing.T) {
 	client := getClient(t)
@@ -15,6 +17,28 @@ func TestLocationOptions(t *testing.T) {
 		},
 		LocationOptions: &LocationOptions{
 			Location: "seattle",
+		},
+	}
+	result, err := client.doSearch(options)
+	check(t, err)
+	assert(t, len(result.Businesses) > 0, CONTAINS_RESULTS)
+}
+
+/**
+ * Check using location options with bounding coordinates
+ */
+func TestLocationWithCoordinates(t *testing.T) {
+	client := getClient(t)
+	options := SearchOptions{
+		GeneralOptions: &GeneralOptions{
+			Term: "food",
+		},
+		LocationOptions: &LocationOptions{
+			"bellevue",
+			&CoordinateOptions{
+				Latitude:  null.FloatFrom(37.788022),
+				Longitude: null.FloatFrom(-122.399797),
+			},
 		},
 	}
 	result, err := client.doSearch(options)
